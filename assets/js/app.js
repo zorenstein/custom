008a9fb4ef778989ae23e98d43dab446b7ce60d4
Matthew Robitaille <matthew@robitaille.io>
2015-06-16 10:31:10 -0400

    Starting setup of build system

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+require('./lib/bootstrap/collapse.js');
+require('./lib/bootstrap/transition.js');
+require('../vendor/jquery-cycle2/build/jquery.cycle2');
+require('../vendor/jquery-cycle2/build/plugin/jquery.cycle2.swipe.min');
+require('./lib/home-cover.js');
+require('./lib/home-slider.js');
+require('./lib/toggle-subnav.js');
+require('./lib/jquery.magnific.js');
+require('./lib/gallery.js');
+require('./lib/footer-nav.js');
+
+require('browsernizr/test/css/flexbox');
+require('browsernizr/test/css/flexboxlegacy');
+require('browsernizr/test/svg');
+
+require('browsernizr');
